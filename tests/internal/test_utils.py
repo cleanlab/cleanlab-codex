b5f71bbe1005414d221d36e2fddde340dcec846e
Angela Liu <angela@cleanlab.ai>
2025-01-28 16:12:18 -0800

    verify api/access key on client init (#15)

--- tests/internal/test_utils.py
+++ tests/internal/test_utils.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from cleanlab_codex.internal.utils import init_codex_client, is_access_key
 
@@ -12,14 +12,18 @@ def test_is_access_key() -> None:
 
 
 def test_init_codex_client_access_key() -> None:
-    with patch("cleanlab_codex.internal.utils._Codex", autospec=True) as mock_codex:
+    mock_client = MagicMock()
+    with patch("cleanlab_codex.internal.utils._Codex", autospec=True, return_value=mock_client) as mock_init:
+        mock_client.projects.access_keys.retrieve_project_id.return_value = "test_project_id"
         client = init_codex_client(DUMMY_ACCESS_KEY)
-        mock_codex.assert_called_once_with(access_key=DUMMY_ACCESS_KEY)
+        mock_init.assert_called_once_with(access_key=DUMMY_ACCESS_KEY)
         assert client is not None
 
 
 def test_init_codex_client_api_key() -> None:
-    with patch("cleanlab_codex.internal.utils._Codex", autospec=True) as mock_codex:
+    mock_client = MagicMock()
+    with patch("cleanlab_codex.internal.utils._Codex", autospec=True, return_value=mock_client) as mock_init:
+        mock_client.users.myself.api_key.retrieve.return_value = "test_project_id"
         client = init_codex_client(DUMMY_API_KEY)
-        mock_codex.assert_called_once_with(api_key=DUMMY_API_KEY)
+        mock_init.assert_called_once_with(api_key=DUMMY_API_KEY)
         assert client is not None
