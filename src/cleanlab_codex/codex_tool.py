4b02d57771d202ee92dc2b5e0d676adc887233e0
Ulyana <ulya.tkach@gmail.com>
2025-02-06 14:11:37 -0800

    [Codex Integrations] Add lanchain + aws integrations (#17)

--- src/cleanlab_codex/codex_tool.py
+++ src/cleanlab_codex/codex_tool.py
@@ -111,3 +111,37 @@ class CodexTool:
                 tool_properties=self._tool_properties,
             ),
         )
+
+    def to_langchain_tool(self) -> Any:
+        """Converts the tool to a [LangChain tool](https://python.langchain.com/docs/concepts/tools/).
+
+        Note: You must have the [`langchain` library installed](https://python.langchain.com/docs/concepts/architecture/) to use this method.
+        """
+        from langchain_core.tools.structured import StructuredTool
+
+        from cleanlab_codex.utils.langchain import create_args_schema
+
+        return StructuredTool.from_function(
+            func=self.query,
+            name=self._tool_name,
+            description=self._tool_description,
+            args_schema=create_args_schema(
+                name=self._tool_name,
+                func=self.query,
+                tool_properties=self._tool_properties,
+            ),
+        )
+
+    def to_aws_converse_tool(self) -> Any:
+        """Converts the tool to an [AWS Converse API tool](https://docs.aws.amazon.com/bedrock/latest/userguide/tool-use-inference-call.html).
+
+        Note: You must have the [`boto3` library installed](https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html) (AWS SDK for Python) to use this method.
+        """
+        from cleanlab_codex.utils.aws import format_as_aws_converse_tool
+
+        return format_as_aws_converse_tool(
+            tool_name=self._tool_name,
+            tool_description=self._tool_description,
+            tool_properties=self._tool_properties,
+            required_properties=self._tool_requirements,
+        )
---
+++ src/cleanlab_codex/utils/langchain.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from inspect import signature
+from typing import Any, Callable, Dict
+
+from pydantic import BaseModel, Field
+
+
+def create_args_schema(name: str, func: Callable[..., Any], tool_properties: Dict[str, Any]) -> type[BaseModel]:
+    """
+    Creates a pydantic BaseModel for langchain's args_schema.
+
+    Args:
+        name: Name of the schema.
+        func: The function for which the schema is being generated.
+        tool_properties: Metadata about each argument.
+
+    Returns:
+        type[BaseModel]: A pydantic model, annotated as required by langchain.
+    """
+    fields = {}
+    params = signature(func).parameters
+
+    for param_name, param in params.items():
+        param_type = param.annotation if param.annotation is not param.empty else Any
+        param_default = param.default
+        description = tool_properties.get(param_name, {}).get("description", None)
+
+        if param_default is param.empty:
+            fields[param_name] = (param_type, Field(description=description))
+        else:
+            fields[param_name] = (
+                param_type,
+                Field(default=param_default, description=description),
+            )
+
+    return type(
+        name,
+        (BaseModel,),
+        {
+            "__annotations__": {k: v[0] for k, v in fields.items()},
+            **{k: v[1] for k, v in fields.items()},
+        },
+    )
