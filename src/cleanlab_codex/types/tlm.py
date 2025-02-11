bdf8872f7b6bf42b4b687b10bfdb77020736f676
Elías Snorrason <eliassno@gmail.com>
2025-02-11 22:56:29 +0000

    formatting

--- src/cleanlab_codex/types/tlm.py
+++ src/cleanlab_codex/types/tlm.py
@@ -1,6 +1,5 @@
 """Protocol for a Trustworthy Language Model."""
 
-
 from __future__ import annotations
 
 from typing import Any, Dict, Protocol, Sequence, Union, runtime_checkable
--- tests/test_codex_backup.py
+++ tests/test_codex_backup.py
@@ -14,6 +14,7 @@ class MockApp:
         # Just echo the user message
         return user_message
 
+
 @pytest.fixture
 def mock_app() -> MockApp:
     return MockApp()
