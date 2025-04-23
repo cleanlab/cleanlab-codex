f19a12d27a7ad1e7db4fd2963ef979a2f360d6d5
Kelsey Wong <kelsey.wong@cleanlab.ai>
2025-04-23 10:28:41 -0700

    increment project query count for manager features POC

--- src/cleanlab_codex/validator.py
+++ src/cleanlab_codex/validator.py
@@ -140,6 +140,9 @@ class Validator:
                 final_metadata.update(processed_metadata)
             expert_answer = self._remediate(query=query, metadata=final_metadata)
 
+        # increment metric for project analytics
+        self._project.increment_query_count()
+
         return {
             "expert_answer": expert_answer,
             "is_bad_response": is_bad_response,
@@ -182,6 +185,9 @@ class Validator:
         expert_answer = None
         if is_bad_response:
             expert_answer = self._remediate(query=query, metadata=final_metadata)
+
+        # increment metric for project analytics
+        self._project.increment_query_count()
 
         return {
             "expert_answer": expert_answer,
